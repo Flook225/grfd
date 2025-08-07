cb869e4b277db5b6298c0c502bc3835837438ef9
Flook225 <flookf225@gmail.com>
2025-08-07 18:28:28 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
+#define RED 2
+#define YELLOW 3
+#define GREEN 4
+
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(YELLOW, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(GREEN, HIGH);
+    delay(10000); // 10 วินาที
+    digitalWrite(GREEN, LOW);
+    digitalWrite(YELLOW, HIGH);
+    delay(2000); // 2 วินาที
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, HIGH);
+    delay(12000); // 12 วินาที
+    digitalWrite(RED, LOW);
+}basic.forever(function () {
 	
 })
